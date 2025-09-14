9fd426722048deb6ccc0277bf432e7ddb547c682
ltetraul <143654006+ltetraul@users.noreply.github.com>
2025-09-14 11:22:50 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -13,3 +13,12 @@ print(ways(12))
 print(ways(20))
 print(ways(3))
 print(ways(0))
+
+def ways(cents, coin_types=[1, 5]):
+    #dynamic approach
+    dp = [0] * (cents + 1)
+    dp[0] = 1
+    for coin in coin_types:
+        for i in range(coin, cents + 1):
+            dp[i] += dp[i - coin]
+    return dp[cents]
